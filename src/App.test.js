5fa2339e1559c9a2b76504a9704c91d9d480fb69
Angel Cuenca <angelscrf@gmail.com>
2018-08-24 12:09:09 -0500

    Creating test case scenarios

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const state = {
+  turnData: {
+      sports: ['Tennis', 'Nfl', 'Nba', 'Fifa'],
+      athlete:{
+        name: 'Roger Federer',
+        imageUrl: 'images/federer.jpg',
+        imageSource: 'Wikimedia Commons',
+        sports: ['Tennis']
+      },      
+  },
+  highlight: 'none'
+};
+
+describe("App", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App data={state.turnData} highlight={state.highlight} onAnswerSelected={()=>{}} />, div); 
+  });
 });
